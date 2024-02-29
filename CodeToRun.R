0320dd9c5fcb1afe9205846cfee6b7fa96f5b6f2
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-02-29 14:26:03 +0000

    Step1-calculate overall incidence

--- CodeToRun.R
+++ CodeToRun.R
@@ -6,20 +6,45 @@ library("PatientProfiles")
 library("IncidencePrevalence")
 
 cdm <- mockDrugUtilisation(n = 10000)
-concept_id_1  <- list("concept" = 378253)
-cohort_name_1 <- "random_1"
-concept_id_2  <- list("concept" = 317009)
-cohort_name_2 <- "random_2"
+denominator_concept_id <- list("concept" = 378253)
+denominator_cohort  <- "random_1"
+outcome_concept_id  <- list("concept" = 317009)
+outcome_cohort <- "random_2"
 
 # Create the general cohort
 cdm <- generateConceptCohortSet(cdm,
-                                conceptSet = concept_id_1,
-                                name = cohort_name_1,
-                                overwrite = TRUE)
+                                conceptSet = denominator_concept_id,
+                                name       = denominator_cohort,
+                                overwrite  = TRUE)
 
 # Create a cohort with the condition under study
 cdm <- generateConceptCohortSet(cdm,
-                                conceptSet = concept_id_2,
-                                name = cohort_name_2,
-                                overwrite = TRUE
+                                conceptSet = outcome_concept_id,
+                                name       = outcome_cohort,
+                                overwrite  = TRUE
 )
+
+# Calculate overall incidence --------------------------------------------------
+incidence_table <- estimateIncidence(
+  cdm,
+  denominatorTable = denominator_cohort,
+  outcomeTable     = outcome_cohort,
+  denominatorCohortId = NULL,
+  outcomeCohortId     = NULL,
+  interval = "overall",
+  completeDatabaseIntervals = TRUE,
+  outcomeWashout = Inf,
+  repeatedEvents = FALSE,
+  minCellCount   = 5,
+  strata = list(),
+  includeOverallStrata = TRUE,
+  temporary = TRUE, 
+  returnParticipants = FALSE
+)
+
+
+# Calculate incidence using achilles -------------------------------------------
+
+
+
+
